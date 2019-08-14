a53115175f09b8f90140c29d320b1df8917eaf9d
lengjian <layer_james@163.com>
2019-08-14 11:21:50 +0800

    add audioScreenDemo

---
+++ lib/Moudle/Home/AudioScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(AudioScreen());
+
+class AudioScreen extends StatefulWidget {
+  @override
+  State createState() => _AudioScreenState();
+}
+
+class _AudioScreenState extends State<AudioScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
