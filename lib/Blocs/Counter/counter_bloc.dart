99a12d83d985f7d1582a289c8ccaa3afc6a8b0d7
lengjian <layer_james@163.com>
2019-08-22 16:48:28 +0800

    change the counter Demo

--- lib/Blocs/Counter/counter_bloc.dart
+++ lib/Blocs/Counter/counter_bloc.dart
@@ -6,17 +6,11 @@ class CounterBloc extends Bloc<CounterEvent, int> {
   get initialState => 0;
 
   @override
-  void onTransition(Transition<CounterEvent, int> transition) {
-    // TODO: implement onTransition
-    super.onTransition(transition);
-    print(transition);
-  }
-  @override
   Stream<int> mapEventToState(CounterEvent event) async* {
     if (event == CounterEvent.Increment) {
       yield currentState + 1;
     } else if (event == CounterEvent.Decrement) {
-      // yield currentState - 1;
+      yield currentState - 1;
     }
   }
 }
